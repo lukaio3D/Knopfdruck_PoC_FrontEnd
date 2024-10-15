0cc3e6b7b8fc6963993cb37561264256ad3b0c45
Lukas Schaab <lukas.schaab@lukaio.de>
2024-10-15 16:43:01 +0200

    updated coordinates of POIs

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 /* import { POIs } from "./POIs";
  */
 const POIs = [
-    { name: "Schloss Johannisburg", lat: 49.975278, lon: 9.136389 },
-    { name: "Pompejanum", lat: 49.975278, lon: 9.136389 },
-    { name: "Stiftsbasilika St. Peter und Alexander", lat: 49.9739, lon: 9.1483 },
-    { name: "Park Schönbusch", lat: 49.9642, lon: 9.1184 },
-    { name: "Museumsdepot", lat: 49.9735, lon: 9.1480 },
-    { name: "Kunsthalle Jesuitenkirche", lat: 49.9737, lon: 9.1481 },
-    { name: "Stadttheater Aschaffenburg", lat: 49.9732, lon: 9.1485 },
-    { name: "Schönborner Hof", lat: 49.9731, lon: 9.1482 },
-    { name: "Gentilhaus", lat: 49.9730, lon: 9.1484 },
-    { name: "Nilkheimer Park", lat: 49.9611, lon: 9.1325 }
+    { name: "Schloss Johannisburg", lat: 49.976424, lon: 9.14154 },
+    { name: "Pompejanum", lat: 49.97755, lon: 9.13641 },
+    { name: "Stiftsbasilika St. Peter und Alexander", lat: 49.97369, lon: 9.14630 },
+    { name: "Park Schönbusch", lat: 49.95682, lon: 9.10422 },
+    { name: "Kunsthalle Jesuitenkirche", lat: 49.97540, lon: 9.14390 },
+    { name: "Stadttheater Aschaffenburg", lat: 49.97424, lon: 9.14426 },
+    { name: "Schönborner Hof", lat: 49.97409, lon: 9.14798 },
+    { name: "Gentilhaus", lat: 49.97544, lon: 9.15975 },
+    { name: "Nilkheimer Park", lat: 49.95276, lon: 9.11511 }
 ];
 // Create a map centered on Aschaffenburg
 let map = L.map("map").setView([49.973, 9.154], 13);
